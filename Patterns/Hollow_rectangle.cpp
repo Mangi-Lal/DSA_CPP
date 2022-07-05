19ccf84e625ed6aa6acd94736dad011561763bcb
mangilal-036 <mangilal1036@gmail.com>
2022-07-05 08:33:22 +0530

    Patterns directory added to git

---
+++ Patterns/Hollow_rectangle.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int rows,col;
+    cin>>rows>>col;
+
+    for(int i=1;i<=rows;i++){
+        for(int j=1;j<=col;j++){
+            if ( i==1 || i==rows || j==1 || j==col ){
+                cout<<" * ";
+            }
+            else
+                cout<<"   ";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
