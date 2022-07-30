2575370a788f4d4380defba2a9992dd1b1009f1e
mangilal-036 <mangilal1036@gmail.com>
2022-07-30 19:49:43 +0530

    Pattern_question_06 solved

---
+++ Question_practice/Pattern_question_06.cpp
@@ -0,0 +1,30 @@
+/*
+Pattern questioln (06)
+A
+B C 
+D E F
+G H I J
+*/
+// ################################################################
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int i = 1;
+    char count = 'A';
+    while (i<=n)
+    {
+        int j = 1;
+        while (j<=i)
+        {
+            cout<<count<<" ";
+            count++;
+            j++;
+        }
+        cout<<"\n";
+        i++;
+    }
+}
