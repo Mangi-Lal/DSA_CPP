0d7609891e50710feaf5bb6dc4b72daea8dd24ff
mangilal-036 <mangilal1036@gmail.com>
2022-12-17 15:35:27 +0530

    String is palindrome or not

---
+++ Recursion/String_is_palindrome.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+bool palindrome(int i, string &str){
+    if(i>=str.size()/2)
+        return true;
+    if(str[i] != str[str.size()-i-1])
+        return false;
+    return palindrome(i+1, str);
+}
+
+int main(){
+    string str = "madam";
+    cout << palindrome(0,str);
+    return 0;
+}
