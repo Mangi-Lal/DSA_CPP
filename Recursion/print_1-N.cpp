fcfe7d71b7a8d9a993d01c79c2a0fae8a0e672cc
mangilal-036 <mangilal1036@gmail.com>
2022-12-17 10:04:05 +0530

    printed numbers from 1 to n

---
+++ Recursion/print_1-N.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void name(int i, int n){
+    if(i>n)
+        return;
+    cout<<i<<" ";
+    name(i+1,n);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    name(1,n);
+}
