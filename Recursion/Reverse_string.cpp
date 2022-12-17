9c2d393f48486ab027981ed6a848c7ec9a9993f7
mangilal-036 <mangilal1036@gmail.com>
2022-12-17 12:33:49 +0530

    Reversed string using single parameters

--- Recursion/Reverse_string.cpp
+++ Recursion/Reverse_string.cpp
@@ -1,13 +1,21 @@
 #include<iostream>
 using namespace std;
-
-void rev(int arr[], int s, int e){
+// using two parameters
+/* void rev(int arr[], int s, int e){
     if(s>=e)
         return;
     swap(arr[s],arr[e]);
     rev(arr, s+1, e-1);
 }
+*/
 
+// using single parameters
+void rev(int arr[], int i, int n){
+    if(i>=n/2)
+        return;
+    swap(arr[i],arr[n-i-1]);
+    rev(arr, i+1, n);
+}
 int main(){
     int n;
     cin>>n;
@@ -16,7 +24,8 @@ int main(){
     {
         cin>>arr[i];
     }
-    rev(arr, 0, n-1);
+    // rev(arr, 0, n-1);
+    rev(arr, 0, n);
     for (int i = 0; i < n; i++)
     {
         cout<<arr[i]<<" ";
