f94df2812428e82fcdeef8cfae1c54bea3c4b499
mangilal-036 <mangilal1036@gmail.com>
2022-12-21 20:30:23 +0530

    updated using vector

--- Functions/Fibonacci.cpp
+++ Functions/Fibonacci.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void fibonacci(int n){
+/* void fibonacci(int n){
     int t1=0;
     int t2=1;
     for (int i=1;i<=n;i++){
@@ -13,11 +13,26 @@ void fibonacci(int n){
     return ;
     
 }
+*/
+// Using vector 
+    vector<long long> printFibb(int n) 
+    {
+        //code here
+        vector<long long> v(n);
+        v[0]=v[1]=1;
+        for(int i=2;i<n;i++){
+            v[i]=v[i-1]+v[i-2];
+        }
 
+        for(int i=0;i<n;i++){
+            cout<<v.at(i)<<" ";
+        }
+        return v;
+    }
 int main()
 {
     int n;
     cin>>n;
-    fibonacci(n); 
+    printFibb(n); 
     return 0;
 }
