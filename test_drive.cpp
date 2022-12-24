72f27977a79834ee940c52ef649275f040beae33
mangilal-036 <mangilal1036@gmail.com>
2022-12-24 18:55:30 +0530

    test_drive

---
+++ test_drive.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+    int evenlyDivides(int N){
+        int counter = 0;
+        int num = N;
+        while(N){
+            int digit = N % 10;
+            if(digit == 0){
+                N=N/10;
+                continue;
+            }
+            if((num%digit) == 0)
+                counter++;
+            N=N/10;
+        }
+        cout<<counter;
+    }
+
+    int main(){
+        int N;
+        cin>>N;
+       evenlyDivides(N);
+    
+    }
